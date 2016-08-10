8ebc8cd7eedf52dd607a4fa0371b6eb9f3fb8aa5
Jake Sanders <jake@livewyer.com>
2016-08-10 15:41:30 +0000

    Fixed rendering

--- resource_gzipme.go
+++ resource_gzipme.go
@@ -5,8 +5,10 @@ import (
 	
 	"compress/gzip"
 	"bytes"
+	"encoding/hex"
 	"encoding/base64"
 	"io"
+	"crypto/sha256"
 )
 
 func resourceGzipme() *schema.Resource {
@@ -34,21 +36,41 @@ func createGzipme(d *schema.ResourceData, gzipper interface{}) error {
 	data_in := d.Get("input").(string)
 	gzbuffer := bytes.Buffer{}
 	gzw := gzip.NewWriter(&gzbuffer)
-	gzw.Write([]byte(data_in))
+	if _, err := gzw.Write([]byte(data_in)); err != nil {
+	    return err
+	}
 	gzw.Close()
 	
 	gzr := bytes.NewReader(gzbuffer.Bytes())
 	b64buffer := bytes.Buffer{}
 	b64w := base64.NewEncoder(base64.StdEncoding, &b64buffer)
-	io.Copy(b64w, gzr)
+	if _, err := io.Copy(b64w, gzr); err != nil {
+	    return err
+	}
 	b64w.Close()
-	
-	d.Set("output", string(b64buffer.Bytes()))
-	return nil
+	d.SetId(hash(string(b64buffer.Bytes())))
+	return d.Set("output", string(b64buffer.Bytes()))
 }
 
 func readGzipme(d *schema.ResourceData, gzipper interface{}) error {
-	return nil
+	data_in := d.Get("input").(string)
+	gzbuffer := bytes.Buffer{}
+	gzw := gzip.NewWriter(&gzbuffer)
+	if _, err := gzw.Write([]byte(data_in)); err != nil {
+	    return err
+	}
+	gzw.Close()
+	
+	gzr := bytes.NewReader(gzbuffer.Bytes())
+	b64buffer := bytes.Buffer{}
+	b64w := base64.NewEncoder(base64.StdEncoding, &b64buffer)
+	if _, err := io.Copy(b64w, gzr); err != nil {
+	    return err
+	}
+	b64w.Close()
+	
+	d.SetId(hash(string(b64buffer.Bytes())))
+	return d.Set("output", string(b64buffer.Bytes()))
 }
 
 func updateGzipme(d *schema.ResourceData, gzipper interface{}) error {
@@ -64,11 +86,15 @@ func updateGzipme(d *schema.ResourceData, gzipper interface{}) error {
 	io.Copy(b64w, gzr)
 	b64w.Close()
 	
-	d.Set("output", string(b64buffer.Bytes()))
-	return nil
+	d.SetId(hash(string(b64buffer.Bytes())))
+	return d.Set("output", string(b64buffer.Bytes()))
 }
 
 func deleteGzipme(d *schema.ResourceData, gzipper interface{}) error {
 	return nil
 }
 
+func hash(s string) string {
+	sha := sha256.Sum256([]byte(s))
+	return hex.EncodeToString(sha[:])
+}
